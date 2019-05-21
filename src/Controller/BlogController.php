c2645c93e17c002329c3a52c5dafbc05706a727d
renaud-pierre <renaudbrecl@gmail.com>
2019-05-21 16:02:44 +0200

    Q3

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'owner' => 'Thomas',
+        ]);
+    }
+}
