cfa4221e21efa3e1fab81b31e7711aeacbd4762a
brecl <retm@gmx.fr>
2019-06-21 16:54:43 +0200

    07 - Symfony : Récupérer des données stockées avec Doctrine

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -13,10 +13,10 @@ class BlogController extends AbstractController
     /**
      * Show all row from article's entity
      *
-     * @Route("/", name="blog_index")
-     * @return Response|Response
+     * @Route("/", name="index")
+     * @return Response A response instance
      */
-    public function index()
+    public function index(): Response
     {
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
