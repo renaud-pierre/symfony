8b209493dab1f39cb5f84235029f4727bb78a4a4
brecl <retm@gmx.fr>
2019-07-01 11:39:05 +0200

    quete 8

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,87 +1,48 @@
 <?php
-
+// src/Controller/BlogController.php
 namespace App\Controller;
-
-use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Flex\Response;
 
 class BlogController extends AbstractController
 {
     /**
      * Show all row from article's entity
      *
-     * @Route("/", name="index")
-     * @return Response A response instance
+     * @Route("/", name="app_index")
+     * @return \Symfony\Component\HttpFoundation\Response|Response
      */
-    public function index(): Response
+    public function index()
     {
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findAll();
-
         if (!$articles) {
             throw $this->createNotFoundException(
-                'No article found in article\'s table.'
-            );
+                'no article found in article\'s table.');
         }
-
-        return $this->render(
-            'blog/index.html.twig',
-            ['articles' => $articles]
-        );
+        return $this->render('blog/index.html.twig', ['articles' => $articles]);
     }
-
     /**
-     * @Route("/blog/list/{page}", requirements={"page"="\d+"}, name="blog_list")
+     * @Route("/blog/show/", name="blog_list")
      */
-    public function list($page)
+    public function Default()
     {
-        return $this->render('blog/list.html.twig',
-            ['page' => $page]);
+        return $this->render('blog/list.html.twig');
     }
-
     /**
-     * Getting a article with a formatted slug for title
-     *
-     * @param string $slug The slugger
-     *
-     * @Route("/{slug<^[a-z0-9-]+$>}",
-     *     defaults={"slug" = null},
-     *     name="blog_show")
-     *  @return Response A response instance
+     * @Route("/blog/show/{slug}",
+     * requirements={"slug"="[a-z0-9-]+"},
+     * defaults={"slug"="Article sans Titre"},
+     * name="blog_show")
      */
-    public function show(?string $slug) : Response
+    public function show($slug)
     {
-        if (!$slug) {
-            throw $this
-                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
-        }
-
-        $slug = preg_replace(
-            '/-/',
-            ' ', ucwords(trim(strip_tags($slug)), "-")
-        );
-
-        $article = $this->getDoctrine()
-            ->getRepository(Article::class)
-            ->findOneBy(['title' => mb_strtolower($slug)]);
-
-        if (!$article) {
-            throw $this->createNotFoundException(
-                'No article with '.$slug.' title, found in article\'s table.'
-            );
-        }
-
-        return $this->render(
-            'blog/show.html.twig',
-            [
-                'article' => $article,
-                'slug' => $slug,
-            ]
-        );
+        // redirection vers la page blog show
+        return $this->render('blog/show.html.twig', ['slug' => $slug = ucwords(str_replace('-', ' ', $slug))]);
     }
     /**
      * @Route("/blog/error/{slug}",
@@ -91,6 +52,18 @@ class BlogController extends AbstractController
      */
     public function error($slug)
     {
+        // redirection vers la page erreur, correspondant à l'insertion de majuscule dans l'URL
         return $this->render('blog/error_404.html.twig', ['slug' => $slug]);
+    }
+    /**
+     * Show all row from article's entity
+     *
+     * @Route("/blog/category/{name}", name="show_category")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showByCategory(Category $category)
+    {
+        $article = $category->getArticles();
+        return $this->render('blog/category.html.twig', ['category' => $category, 'articles' => $article]);
     }
 }
